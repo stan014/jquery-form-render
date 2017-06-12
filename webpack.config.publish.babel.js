3444650640d717866130fa7e9bad29b8d2464280
stanley <stanley@kiri.tech.com>
2017-06-12 10:19:28 +0800

    setup package  publish setting

--- webpack.config.publish.babel.js
+++ webpack.config.publish.babel.js
@@ -1,13 +1,21 @@
-import path from 'path';
-import webpack from 'webpack';
+import path from 'path'
+import webpack from 'webpack'
+const {UglifyJsPlugin} = webpack.optimize
+
 export default {
     entry : [path.join(__dirname, 'src/index.js')],
     target: 'node',
     output : {
         path: path.join(__dirname, 'dist'),
         filename: 'jquery-form-render.js',
-        publicPath: '/dist'
+        publicPath: '/dist',
+        library:'jquery-form-render',
+        libraryTarget:'umd',
+        umdNamedDefine:true
     },
+    plugins:[
+      new UglifyJsPlugin({minimize:true})
+    ],
     module : {
         loaders: [
             {
